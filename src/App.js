0b37fcd57c58a9cb6513bebe63e0e27e5702553c
thvrijn <thvrijn2002@gmail.com>
2023-01-25 23:24:13 +0100

    add deg logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+
+import { useEffect, useRef } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	const canvas = useRef(null)
 
-export default App;
+	useEffect(() => {
+		var ctx = canvas.current.getContext("2d");
+
+		canvas.current.height = window.innerHeight
+		canvas.current.width = window.innerWidth
+		ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
+
+		ctx.strokeRect(0, 0, 50, 50)
+
+		var angle = 90
+		var x1 = 25
+		var y1 = 25
+		var length = 200
+
+		var x2 = x1 + Math.cos(Math.PI * (angle - 90) / 180) * length
+		var y2 = y1 + Math.sin(Math.PI * (angle - 90) / 180) * length
+
+		ctx.strokeRect(x2 - 25, y2 - 25, 50, 50)
+	}, [])
+
+	return <div>
+		<canvas ref={canvas} className={'canvas'}>
+
+		</canvas>
+	</div>
+}
